7771ed20706f04601a8cf777472b8d9e7902788b
Gongjjin <gjyj5395@naver.com>
2025-08-25 16:39:12 +0900

    chore: 주석 docs 추가

--- src/main/java/WishPool/Be/wishpoool/domain/WishPoolStatus.java
+++ src/main/java/WishPool/Be/wishpoool/domain/WishPoolStatus.java
@@ -1,6 +1,12 @@
 package WishPool.Be.wishpoool.domain;
 
 public enum WishPoolStatus {
+    /**
+     * ACTIVE = 참여자
+     * CANCELED = 취소된 위시풀
+     * GIFT_SELECTION = 생일자
+     * CLOSED = 완료된 위시풀
+     */
     ACTIVE,
     CANCELED,
     GIFT_SELECTION,
