6a1d187d5986da1ae119f7fefa1976e8d9d25a9b
Gongjjin <gjyj5395@naver.com>
2025-08-25 13:32:48 +0900

    feat: dotenv 제거 후 EnableJpaAuditing 어노테이션 추가

--- src/main/java/WishPool/Be/BeApplication.java
+++ src/main/java/WishPool/Be/BeApplication.java
@@ -1,12 +1,13 @@
 package WishPool.Be;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableJpaAuditing
 public class BeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BeApplication.class, args);
