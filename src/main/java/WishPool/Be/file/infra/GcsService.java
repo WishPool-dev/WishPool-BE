d52516efe18de8ef095d436abf67e290f200ddd9
Gongjjin <gjyj5395@naver.com>
2025-08-25 13:37:25 +0900

    feat: 파일 삭제, url 전환 메소드 작성

--- src/main/java/WishPool/Be/file/infra/GcsService.java
+++ src/main/java/WishPool/Be/file/infra/GcsService.java
@@ -1,9 +1,12 @@
-package WishPool.Be.gcs;
+package WishPool.Be.file.infra;
 
-import WishPool.Be.gcs.exception.GcsUploadException;
+import WishPool.Be.file.application.service.FileService;
+import WishPool.Be.file.exception.GcsUploadException;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,13 +17,14 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class GcsService {
+public class GcsService implements FileService {
 
     private final Storage storage;
 
     @Value("${spring.cloud.gcp.storage.bucket}")
     private String bucketName;
 
+    @Override
     @Async // 이 메소드를 별도의 스레드에서 비동기로 실행
     public void uploadImageAsync(MultipartFile file, String key) {
         try {
@@ -38,7 +42,21 @@ public class GcsService {
         }
     }
 
+    // true면 삭제, false면 실패
+    @Override
     public boolean deleteImage(String key) {
         return storage.delete(bucketName, key);
+    }
+
+    @Override
+    public String getImageURL(String key) {
+        BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, key).build();
+        // 15분 동안 유효한 V4 서명의 Presigned URL을 생성
+        return storage.signUrl(
+                blobInfo,
+                15, // URL 유효 시간
+                TimeUnit.MINUTES, // 시간 단위
+                Storage.SignUrlOption.withV4Signature()
+        ).toString();
     }
 }
