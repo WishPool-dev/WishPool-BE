b42b56dea75d05a337657f0a0a77a39deadc26f2
Gongjjin <gjyj5395@naver.com>
2025-08-25 13:40:01 +0900

    feat: api 응답 실패 시 경로 및 시간 정보를 담은 metadata 생성

---
+++ src/main/java/WishPool/Be/common/MetaData.java
@@ -0,0 +1,19 @@
+package WishPool.Be.common;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import org.threeten.bp.LocalDateTime;
+
+/**
+ * http 예외 발생 시 발생 API 경로 및 시간을 표현하는 클래스
+ */
+@Getter
+public class MetaData {
+    private final String path;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private final LocalDateTime timestamp;
+    public MetaData(String path) {
+        this.path = path;
+        this.timestamp = LocalDateTime.now(); // 객체 생성 시점의 현재 시간
+    }
+}
