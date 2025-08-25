3d956a176162a906d6d408e2c0f46caf95a193e1
Gongjjin <gjyj5395@naver.com>
2025-08-25 13:43:12 +0900

    refactor: 기존 복잡한 패키징에서 도메인 중심으로 패키징 전환

---
+++ src/main/java/WishPool/Be/user/domain/UserRepository.java
@@ -0,0 +1,9 @@
+package WishPool.Be.user.domain;
+
+import java.util.Optional;
+
+public interface UserRepository {
+    Optional<User> findByEmail(String email);
+    User save(User user);
+    Optional<User> findById(Long userId);
+}
