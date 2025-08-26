44ca088087fa14523711ffb9268031805a23c214
Gongjjin <gjyj5395@naver.com>
2025-08-26 21:51:50 +0900

    feat: 생일자 선물 확인 엔드포인트 추가

--- src/main/java/WishPool/Be/wishpoool/presentation/WishPoolController.java
+++ src/main/java/WishPool/Be/wishpoool/presentation/WishPoolController.java
@@ -3,6 +3,7 @@ package WishPool.Be.wishpoool.presentation;
 import WishPool.Be.wishpoool.application.dto.request.CreateGiftListRequestDto;
 import WishPool.Be.wishpoool.application.command.WishPoolCommandService;
 import WishPool.Be.wishpoool.application.dto.response.WishPoolGuestInfoResponseDto;
+import WishPool.Be.wishpoool.application.dto.response.gift.BirthdayGiftsViewResponseDto;
 import WishPool.Be.wishpoool.application.dto.response.gift.GiftListResponseDto;
 import WishPool.Be.wishpoool.application.query.GiftListQueryService;
 import WishPool.Be.wishpoool.application.query.WishPoolQueryService;
@@ -27,6 +28,7 @@ public class WishPoolController {
         return ResponseEntity.ok().body(wishPoolQueryService.getGuestInfo(shareIdentifier));
     }
 
+    // 게스트의 위시풀 참여
     @PostMapping("/guests")
     public ResponseEntity<Long> joinWishPoolWithGuest(@RequestBody CreateGiftListRequestDto dto){
         Long createdWishpoolId = wishPoolCommandService.createGiftListForGuest(dto);
@@ -34,8 +36,15 @@ public class WishPoolController {
         return ResponseEntity.created(uri).body(createdWishpoolId);
     }
 
+    // 선물 리스트 확인
     @GetMapping("/gifts/{wishpoolId}")
     public ResponseEntity<List<GiftListResponseDto>> getGiftLists(@PathVariable Long wishpoolId){
         return ResponseEntity.ok().body(giftListQueryService.getAllGifts(wishpoolId));
+    }
+
+    // 생일자가 링크로 선물이랑 문구 받아보기
+    @GetMapping("/celebrant/{chosenIdentifier}")
+    public ResponseEntity<BirthdayGiftsViewResponseDto> joinWishPoolWithGuest(@PathVariable String chosenIdentifier){
+        return ResponseEntity.ok().body(giftListQueryService.getGiftsForBirthdayOwner(chosenIdentifier));
     }
 }
