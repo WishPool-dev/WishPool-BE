895b6b79288accf8db7014bb71e69a1d60e65c7c
Gongjjin <gjyj5395@naver.com>
2025-08-26 15:26:30 +0900

    feat: WishPoolRepository 기능 추가

--- src/main/java/WishPool/Be/wishpoool/domain/WishPoolRepository.java
+++ src/main/java/WishPool/Be/wishpoool/domain/WishPoolRepository.java
@@ -1,9 +1,13 @@
 package WishPool.Be.wishpoool.domain;
 
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.Optional;
 
 public interface WishPoolRepository {
     Optional<WishPool> findById(Long id);
-
     WishPool save (WishPool wishPool);
+    Optional<WishPool> findByShareIdentifier(String shareIdentifier);
+
+    Optional<WishPool> findWithParticipantsAndGiftListById(Long wishpoolId);
 }
