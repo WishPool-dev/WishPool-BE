b630bcc6021deb013bb96467bd6620ac51bfacd1
Gongjjin <gjyj5395@naver.com>
2025-08-26 15:26:07 +0900

    feat: client와 맞추어 상태 이름 변경

--- src/main/java/WishPool/Be/wishpoool/domain/WishPoolStatus.java
+++ src/main/java/WishPool/Be/wishpoool/domain/WishPoolStatus.java
@@ -2,13 +2,11 @@ package WishPool.Be.wishpoool.domain;
 
 public enum WishPoolStatus {
     /**
-     * ACTIVE = 참여자
-     * CANCELED = 취소된 위시풀
-     * GIFT_SELECTION = 생일자
-     * CLOSED = 완료된 위시풀
+     * OPEN = 참여자
+     * WAITING = 생일자
+     * COMPLETED = 완료된 위시풀
      */
-    ACTIVE,
-    CANCELED,
-    GIFT_SELECTION,
-    CLOSED
+    OPEN,
+    WAITING,
+    COMPLETED
 }
