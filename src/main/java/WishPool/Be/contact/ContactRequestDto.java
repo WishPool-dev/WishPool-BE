961a6f82ae545d6df69ad7c1870fe13d003c2c75
Gongjjin <gjyj5395@naver.com>
2025-09-19 13:44:49 +0900

    feat: validation 추가

--- src/main/java/WishPool/Be/contact/ContactRequestDto.java
+++ src/main/java/WishPool/Be/contact/ContactRequestDto.java
@@ -1,9 +1,7 @@
 package WishPool.Be.contact;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
+
 public record ContactRequestDto(
         @NotNull(message = "별점을 설정해주세요.")
         @Min(value = 1, message = "별점은 최소 1점 이상이어야 합니다.")
@@ -11,5 +9,6 @@ public record ContactRequestDto(
         int rating,
 
         @NotBlank(message = "평가하신 내용을 추가해주세요.")
+        @Size(max = 200, message = "내용은 최대 200자까지 입력 가능합니다.")
         String content) {
 }
