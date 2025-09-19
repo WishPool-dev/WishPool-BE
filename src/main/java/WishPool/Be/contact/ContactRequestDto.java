8692fdba00c3902ba40db21ce39424d83dc5bc94
Gongjjin <gjyj5395@naver.com>
2025-09-19 13:43:43 +0900

    feat: 별점 범위 추가

--- src/main/java/WishPool/Be/contact/ContactRequestDto.java
+++ src/main/java/WishPool/Be/contact/ContactRequestDto.java
@@ -1,9 +1,13 @@
 package WishPool.Be.contact;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 public record ContactRequestDto(
         @NotNull(message = "별점을 설정해주세요.")
+        @Min(value = 1, message = "별점은 최소 1점 이상이어야 합니다.")
+        @Max(value = 5, message = "별점은 최대 5점까지 가능합니다.")
         int rating,
 
         @NotBlank(message = "평가하신 내용을 추가해주세요.")
