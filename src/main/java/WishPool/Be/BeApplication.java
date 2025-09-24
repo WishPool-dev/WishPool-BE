72b623faa0e8fb58274382dbb586ece4f31d65a3
Gongjjin <gjyj5395@naver.com>
2025-09-24 18:20:21 +0900

    hotfix: 스웨거 https 설정

--- src/main/java/WishPool/Be/BeApplication.java
+++ src/main/java/WishPool/Be/BeApplication.java
@@ -1,5 +1,7 @@
 package WishPool.Be;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableAsync
 @EnableJpaAuditing
+@OpenAPIDefinition(servers = {@Server(url = "https://api.wishpool.store", description = "Default Server URL")})
 public class BeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BeApplication.class, args);
