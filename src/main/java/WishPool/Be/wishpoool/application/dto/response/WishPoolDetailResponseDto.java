bb262b1748c4f5a181f3e5394512da0c7327cde6
Gongjjin <gjyj5395@naver.com>
2025-09-29 09:53:23 +0900

    feat: 날짜 조회 형식 yyyy-mm에서 yyyy/mm으로 변경

--- src/main/java/WishPool/Be/wishpoool/application/dto/response/WishPoolDetailResponseDto.java
+++ src/main/java/WishPool/Be/wishpoool/application/dto/response/WishPoolDetailResponseDto.java
@@ -3,12 +3,12 @@ package WishPool.Be.wishpoool.application.dto.response;
 import WishPool.Be.wishpoool.domain.WishPool;
 import WishPool.Be.wishpoool.domain.WishPoolStatus;
 
-import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 // 로그인한 사용자의 위시풀 상세 조회
 public record WishPoolDetailResponseDto(
     String imageKey,
-    LocalDate endDate,
+    String endDate, // LocalDate -> String으로 변경
     String celebrant,
     Long joinCount,
     String description,
@@ -17,6 +17,12 @@ public record WishPoolDetailResponseDto(
     WishPoolStatus status
 ){
     public WishPoolDetailResponseDto(WishPool wishPool, Long joinCount, int d_day){
-        this(wishPool.getImageKey(),wishPool.getParticipantEndDate(),wishPool.getCelebrant(), joinCount, wishPool.getDescription(), d_day, wishPool.getWishPoolStatus());
+        this(wishPool.getImageKey()
+                ,wishPool.getParticipantEndDate().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"))
+                ,wishPool.getCelebrant()
+                ,joinCount
+                ,wishPool.getDescription()
+                ,d_day
+                ,wishPool.getWishPoolStatus());
     }
 }
